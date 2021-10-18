6f697b0b15755ab852c49d1947bead0a18a8e936
jfangwang <qbs1864@gmail.com>
2021-10-18 15:12:53 -0400

    converted to spaces

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,5 +4,5 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-	"""returns their sum as a float"""
-	return sum(input_list)
+    """returns their sum as a float"""
+    return sum(input_list)
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,5 +4,5 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-	"""returns the sum as a float"""
-	return sum(mxd_list)
+    """returns the sum as a float"""
+    return sum(mxd_list)

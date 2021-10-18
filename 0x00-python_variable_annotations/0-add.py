02322d6a18891e167d3a8b1a01a9f22effa2636f
jfangwang <qbs1864@gmail.com>
2021-10-18 14:46:42 -0400

    added comment

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+""" Basic annotations - add """
 
 
 def add(a: float, b: float) -> float:
+    """static typed function takes in two floats and return the sum"""
     return a + b

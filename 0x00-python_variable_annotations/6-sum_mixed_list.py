587e66e4aa683f6866b75448b3904f2d16a99a23
jfangwang <qbs1864@gmail.com>
2021-10-18 15:13:56 -0400

    fixed spelling error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,4 +5,4 @@ from typing import List, Union
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """returns the sum as a float"""
-    return sum(mxd_list)
+    return sum(mxd_lst)

0e93a59b34f6a664e8a4492be96efd19b7665d1b
jfangwang <qbs1864@gmail.com>
2021-10-19 21:13:47 -0400

    4 done

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Basic annotation: tasks"""
+import asyncio
+from typing import List
+task_wait_r = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Take the code from wait_n and alter it into a new function task_wait_n.
+    The code is nearly identical to wait_n except task_wait_random is being called."""
+    delay_list: List[float] = []
+    sorted_list: List[float] = []
+
+    for index in range(0, n):
+        delay_list.append(task_wait_r(max_delay))
+    for item in asyncio.as_completed(delay_list):
+        """Waits until item is complete and stores into sorted list"""
+        result = await item
+        sorted_list.append(result)
+    return sorted_list

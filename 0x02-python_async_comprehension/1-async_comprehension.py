f34dd4ab372bbb295fea0efe3b879c4b5a302866
jfangwang <qbs1864@gmail.com>
2021-10-20 11:14:33 -0400

    fixed python annotations

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,11 +2,11 @@
 """Basic annotation: async comprehension"""
 import asyncio
 import random
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """The coroutine will loop 10 times, each time
     asynchronously wait 1 second, then yield a random
     number between 0 and 10. Use the random module."""

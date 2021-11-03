2488dd34229d7f63ccb1521e9a0031901c498383
jfangwang <qbs1864@gmail.com>
2021-11-03 15:55:17 -0400

    can get user based on email and password

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Basic Authentication"""
 from api.v1.auth.auth import Auth
+from typing import TypeVar
+from models.user import User
 import base64
 
 
@@ -39,3 +41,16 @@ class BasicAuth(Auth):
         mid = decoded_base64_authorization_header.index(":")
         return (decoded_base64_authorization_header[0:mid],
                 decoded_base64_authorization_header[mid + 1:])
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """returns the User instance based on his email and password"""
+        if user_email is None or not isinstance(user_email, str) or\
+                user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        all_users = User.search({'email': user_email})
+        for user in all_users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None

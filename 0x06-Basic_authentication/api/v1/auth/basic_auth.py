0da6de1ebb09fd4729a59cc9166207522240bc85
jfangwang <qbs1864@gmail.com>
2021-11-03 15:20:50 -0400

    added basic keyword

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,8 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """returns the base64 part of the authorization"""
+        if authorization_header is None or\
+            not isinstance(authorization_header, str) or\
+                not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]

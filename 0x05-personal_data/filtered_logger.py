00838881229affa583dd8ac3bf0adc3370fd03d4
jfangwang <qbs1864@gmail.com>
2021-11-05 16:35:46 -0400

    done

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Regex"""
 from typing import List
-
+import re
 
 def filter_datum(fields: List[str], redaction, message: str, separator: str):
     """returns the log message obfuscated"""
-    return "{}".format(fields)
+    for entry in fields:
+        message = re.sub(f"{entry}=.+?{separator}", f"{entry}={redaction}{separator}", message)
+    return message

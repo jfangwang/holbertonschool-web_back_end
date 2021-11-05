f9138a63d3a3f34ed927a9394941748e61d55f5c
jfangwang <qbs1864@gmail.com>
2021-11-05 16:36:20 -0400

    pycodestyle compliant

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -3,8 +3,10 @@
 from typing import List
 import re
 
+
 def filter_datum(fields: List[str], redaction, message: str, separator: str):
     """returns the log message obfuscated"""
     for entry in fields:
-        message = re.sub(f"{entry}=.+?{separator}", f"{entry}={redaction}{separator}", message)
+        message = re.sub(f"{entry}=.+?{separator}",
+                         f"{entry}={redaction}{separator}", message)
     return message

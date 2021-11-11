096a71d45875bb9fc79ae60680e55c8931f5de2d
jfangwang <qbs1864@gmail.com>
2021-11-10 21:35:41 -0500

    1 done

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Session Authentication"""
+from flask import request
+from typing import List, TypeVar
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """class Session Auth"""

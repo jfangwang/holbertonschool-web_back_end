2c25b2a5f1577efa8fe4995353d407c4b5ac9385
jfangwang <qbs1864@gmail.com>
2021-11-10 21:57:30 -0500

    something different

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -60,7 +60,7 @@ def before_request() -> str:
         return
     if auth.authorization_header(request) is None:
         abort(401)
-    if auth.authorization_header(request) and\
+    if auth.authorization_header(request) or\
             auth.session_cookie(request):
         abort(401)
     if auth.current_user(request) is None:

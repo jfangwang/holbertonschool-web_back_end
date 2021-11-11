e802dfab6cef82e6a1e949c526c1016d4a497d4a
jfangwang <qbs1864@gmail.com>
2021-11-10 21:31:10 -0500

    task 0 done{

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -20,9 +20,6 @@ if AUTH_TYPE == 'auth':
 elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif AUTH_TYPE ==  'sesssion_auth':
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
 
 
 @app.errorhandler(404)
@@ -54,12 +51,10 @@ def before_request() -> str:
         return
     forbidden = ['/api/v1/status/',
                  '/api/v1/unauthorized/',
-                 '/api/v1/forbidden/',
-                 '/api/v1/auth_session/login/']
+                 '/api/v1/forbidden/']
     if auth.require_auth(request.path, forbidden) is False:
         return
-    if auth.authorization_header(request) is None and\
-        auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -2,7 +2,6 @@
 """Authentication"""
 from flask import request
 from typing import List, TypeVar
-from os import getenv
 
 
 class Auth():
@@ -32,10 +31,3 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """returns the flask request object"""
         return None
-
-    def session_cookie(self, request=None):
-        """that returns a cookie value from a request"""
-        if request is None:
-            return None
-        session = getenv("SESSION_NAME")
-        return request.cookies.get(session)

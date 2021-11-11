5435d3493f24024b1c5e745dc70389362639ffc1
jfangwang <qbs1864@gmail.com>
2021-11-10 21:51:31 -0500

    task 5 done

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -54,13 +54,17 @@ def before_request() -> str:
         return
     forbidden = ['/api/v1/status/',
                  '/api/v1/unauthorized/',
-                 '/api/v1/forbidden/']
+                 '/api/v1/forbidden/',
+                 '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, forbidden) is False:
         return
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    if auth.authorization_header(request) is None and\
+        auth.session_cookie(request) is None:
+        abort(401)
     request.current_user = auth.current_user(request)
 
 

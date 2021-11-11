cbc266dc04eb721a87be6d6d8e3b6327d0f34701
jfangwang <qbs1864@gmail.com>
2021-11-10 21:48:11 -0500

    pycodestyle compliant

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -32,7 +32,7 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """returns the flask request object"""
         return None
-    
+
     def session_cookie(self, request=None):
         """that returns a cookie value from a request"""
         if request is None:

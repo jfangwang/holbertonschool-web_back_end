82e39fca8804b38ed5fbf4bbfbd09d8b727bd68a
jfangwang <qbs1864@gmail.com>
2021-11-10 22:00:58 -0500

    fixed correct abort messages

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -58,10 +58,8 @@ def before_request() -> str:
                  '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, forbidden) is False:
         return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if not auth.authorization_header(request) and\
-            not auth.session_cookie(request):
+    if auth.authorization_header(request) is None and\
+            auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)

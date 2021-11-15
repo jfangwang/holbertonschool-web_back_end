7da5be2adeec38361101f4d16af9f9a578184048
jfangwang <qbs1864@gmail.com>
2021-11-15 10:22:41 -0500

    added user model

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Model: User"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 Base = declarative_base()
@@ -6,4 +7,9 @@ Base = declarative_base()
 
 class User(Base):
     """SQLAlchemy model"""
-
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))

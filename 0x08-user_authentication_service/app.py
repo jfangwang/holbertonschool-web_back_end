971cd10a30cbbc30b68d3b871f6f9a73e5b2ee35
jfangwang <qbs1864@gmail.com>
2021-11-15 14:28:25 -0500

    can register users on flask

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 """Flask App"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from auth import Auth
 app = Flask(__name__)
-auth = Auth()
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes="False")
 def default():
     """return a JSON payload of the form"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes="False")
+def users():
+    """API endpoint for registering users"""
+    user = request.form.get("email")
+    pas = request.form.get("password")
+    try:
+        AUTH.register_user(user, pas)
+        return jsonify({"email": "{}", "message": "user create\
+                         d".format(user)}), 200
+    except (NoResultFound):
+        return jsonify({"message": "email already registered"}), 400
+    except Exception(e):
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":

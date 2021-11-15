3be426e545d17f4f5cefd6d27a9c6fdc1f726fa7
jfangwang <qbs1864@gmail.com>
2021-11-15 15:47:07 -0500

    fixed syntax

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -81,7 +81,7 @@ class Auth:
     def destroy_session(self, used_id: int) -> None:
         """updates the corresponding user’s session ID to None"""
         try:
-            self._db.update_user(user_id=user_id,
+            self._db.update_user(user_id,
                                  session_id=None)
         except NoResultFound:
             return None

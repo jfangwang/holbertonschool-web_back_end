e7a8e6f42ed128b57aae84060d53c2c5ce5ce465
jfangwang <qbs1864@gmail.com>
2021-11-15 16:53:35 -0500

    newly created unit test file

--- 0x08-user_authentication_service/main.py
+++ 0x08-user_authentication_service/main.py
@@ -1,14 +1,38 @@
 #!/usr/bin/env python3
 """
 Main file
+Unit test file making sure all functions are behaving correctly
 """
-from auth import Auth
+import requests
 
-email = 'bob@bob.com'
-password = 'MyPwdOfBob'
-auth = Auth()
 
-auth.register_user(email, password)
+def register_user(email: str, password: str) -> None:
+    """Test register user"""
 
-print(auth.create_session(email))
-print(auth.create_session("unknown@email.com"))
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """Test login wrong password"""
+
+
+def log_in(email: str, password: str) -> str:
+    """test log in"""
+
+
+def profile_unlogged() -> None:
+    """test progile unlogged"""
+
+
+def profile_logged(session_id: str) -> None:
+    """Test profile logged"""
+
+
+def log_out(session_id: str) -> None:
+    """test log out"""
+
+
+def reset_password_token(email: str) -> str:
+    """Test Reset password token"""
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """Test update password"""

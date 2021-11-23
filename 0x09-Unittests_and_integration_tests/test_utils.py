9a13f4b202d70f794de34c9bab2a2c4c23e2bcc6
jfangwang <qbs1864@gmail.com>
2021-11-23 13:10:25 -0500

    forgot to import access nested map

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 """Flask App"""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from parameterized import parameterized, parameterized_class
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """class to test cases on utils.py"""
 
     @parameterized.expand([
-        ({"a": 1}, ('a',), 1)
-        ({"a": {"b": 2}}, ('a',), {'b': 2})
-        ({"a": {"b": 2}}, ('a', 'b'), 2)
-    ])
+                          ({"a": 1}, ('a',), 1)
+                          ({"a": {"b": 2}}, ('a',), {'b': 2})
+                          ({"a": {"b": 2}}, ('a', 'b'), 2)
+                          ])
     def test_access_nested_map(self, n_map, path, ans):
-        self.assertEqual(access_nested_map(n_map, path), ans)
+        self.assertEqual(access_nested_map(nested_map, path), ans)
 
 
 if __name__ == '__main__':

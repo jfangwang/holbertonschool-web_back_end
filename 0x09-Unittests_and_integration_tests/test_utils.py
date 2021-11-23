b8bbfc341a3fdc7b053855250a5686d658522110
jfangwang <qbs1864@gmail.com>
2021-11-23 12:18:33 -0500

    first attempt

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 """Flask App"""
+import unittest
+from parameterized import parameterized, parameterized_class
 
+
+class TestAccessNestedMap(unittest.TestCase):
+    """class to test cases on utils.py"""
+
+    @parameterized.expand([
+        ({"a": 1}, ('a',), 1)
+        ({"a": {"b": 2}}, ('a',), {'b': 2})
+        ({"a": {"b": 2}}, ('a', 'b'), 2)
+    ])
+    def test_access_nested_map(self, n_map, path, ans):
+        self.assertEqual(access_nested_map(n_map, path), ans)
+
+
+if __name__ == '__main__':
+    unittest.main()

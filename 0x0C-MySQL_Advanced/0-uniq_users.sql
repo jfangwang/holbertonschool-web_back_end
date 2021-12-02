b5a9ec1a59e6b95dd2493839c55bc104b0f28a42
jfangwang <qbs1864@gmail.com>
2021-12-02 09:42:46 -0500

    added comment

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,6 @@
+-- A SQL script creating table users with id, email, and name
 CREATE TABLE IF NOT EXISTS 'users' (
-    'id' INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    'id' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     'email' VARCHAR(255) NOT NULL UNIQUE,
     'name' VARCHAR(255)
 )

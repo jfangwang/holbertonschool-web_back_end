f314f66b17e799d0b0e95fb83ffc23ceb609b8f1
jfangwang <qbs1864@gmail.com>
2021-12-02 10:13:43 -0500

    1 done

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- A SQL script creating table users with id, email, and name
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);

ec9ef192a65929bf1dbedab86de7f180e088f33d
jfangwang <qbs1864@gmail.com>
2021-12-07 15:13:20 -0500

    added tabs

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ provides some stats about Nginx logs stored in MongoDB """
-import pymongo
 from pymongo import MongoClient
 
 cli = MongoClient()
@@ -8,9 +7,9 @@ log = cli.logs.nginx
 
 print("{} logs".format(log.count_documents({})))
 print("Methods:")
-print("    method GET: {}".format(log.count_documents({"method":"GET"})))
-print("    method POST: {}".format(log.count_documents({"method":"POST"})))
-print("    method PUT: {}".format(log.count_documents({"method":"PUT"})))
-print("    method PATCH: {}".format(log.count_documents({"method":"PATCH"})))
-print("    method DELETE: {}".format(log.count_documents({"method":"DELETE"})))
+print("\tmethod GET: {}".format(log.count_documents({"method":"GET"})))
+print("\tmethod POST: {}".format(log.count_documents({"method":"POST"})))
+print("\tmethod PUT: {}".format(log.count_documents({"method":"PUT"})))
+print("\tmethod PATCH: {}".format(log.count_documents({"method":"PATCH"})))
+print("\tmethod DELETE: {}".format(log.count_documents({"method":"DELETE"})))
 print("{} status check".format(log.count_documents({"method":"GET", "path": "/status"})))

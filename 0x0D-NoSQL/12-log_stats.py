f627209989af81b067dbf59c96753183004bc5d4
jfangwang <qbs1864@gmail.com>
2021-12-07 15:04:41 -0500

    12 doen

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" provides some stats about Nginx logs stored in MongoDB """
+import pymongo
+from pymongo import MongoClient
+
+cli = MongoClient()
+log = cli.logs.nginx
+
+print("{} logs".format(log.count_documents({}))
+print("Methods")
+print("    method GET: {}".format(log.count_documents({"method":"GET")))
+print("    method POST: {}".format(log.count_documents({"method":"POST"})))
+print("    method PUT: {}".format(log.count_documents({"method":"PUT"})))
+print("    method PATCH: {}".format(log.count_documents({"method":"PATCH"})))
+print("    method DELETE: {}".format(log.count_documents({"method":"DELETE"})))
+print("{} status check".format(log.count_documents({"method":"GET", "path": "/status"})))

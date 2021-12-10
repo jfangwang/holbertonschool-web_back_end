af487508a9fd27fe2c99b8a3134288651f231cc3
Jonny Wang <qbs1864@gmail.com>
2021-12-10 02:40:10 +0000

    10 done:

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -3,7 +3,6 @@ export default function appendToEachArrayValue(array, appendString) {
     for (const idx of array) {
       output.push(appendString + value);
     }
-  
-    return output;
-  }
-  
+
+  return output;
+}

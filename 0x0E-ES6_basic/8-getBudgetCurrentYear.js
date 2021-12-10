8cd6ed3906000d1defb6bcafb233291eb05e4ff6
Jonny Wang <qbs1864@gmail.com>
2021-12-10 02:24:17 +0000

    8 fixed

--- 0x0E-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x0E-ES6_basic/8-getBudgetCurrentYear.js
@@ -3,13 +3,13 @@ function getCurrentYear() {
     return date.getFullYear();
   }
   
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const budget = {
-      [`income-${getCurrentYear()}`]: income,
-      [`gdp-${getCurrentYear()}`]: gdp,
-      [`capita-${getCurrentYear()}`]: capita,
-    };
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 
-    return budget;
-  }
+  return budget;
+}
   

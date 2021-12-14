f7039d69d45350bfa18b4de264a2974d18d4ca1b
Jonny Wang <qbs1864@gmail.com>
2021-12-14 19:37:35 +0000

    eslinted

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -4,7 +4,7 @@ function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw Error('cannot divide by 0');
   } else {
-    return (numerator/denominator);
+    return (numerator / denominator);
   }
 }
 export default divideFunction;

714ee9295ee060a17d2b6120b102c31b30aa2994
Jonny Wang <qbs1864@gmail.com>
2021-12-14 19:36:49 +0000

    8 done

---
+++ 0x0F-ES6_promise/8-try.js
@@ -0,0 +1,10 @@
+// return an object on success and error object on failure
+
+function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    return (numerator/denominator);
+  }
+}
+export default divideFunction;

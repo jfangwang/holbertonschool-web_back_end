d2c8c331b51a085d80dfe564d157851d2ba03403
Jonny Wang <qbs1864@gmail.com>
2021-12-14 19:33:14 +0000

    done

--- 0x0F-ES6_promise/7-load_balancer.js
+++ 0x0F-ES6_promise/7-load_balancer.js
@@ -1,6 +1,6 @@
 // return an object on success and error object on failure
 
 function loadBalancer(chinaDownload, USDownload) {
-
+  return Promise.race([chinaDownload, USDownload]);
 }
 export default loadBalancer;

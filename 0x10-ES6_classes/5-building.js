b58f1dd8e1a1460d1593b2ceba4387604864b2d6
Jonny Wang <qbs1864@gmail.com>
2021-12-17 02:05:00 +0000

    linted

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -2,7 +2,7 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-        throw Error('Class extending Building must override evacuationWarningMessage');
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }

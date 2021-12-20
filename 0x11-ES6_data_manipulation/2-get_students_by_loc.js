d9355ab2fd46681abbe694d2a1c955bee5cd0854
Jonny Wang <qbs1864@gmail.com>
2021-12-20 18:24:41 +0000

    2 done

--- 0x11-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x11-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,9 +1,8 @@
-// Implement a class named getListStudents that returns an array of objects
-export default function getListStudents() {
-  const output = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-  return (output);
+// Implement a function getStudentsByLocation
+export default function getStudentsByLocation(array, city) {
+  let output = [];
+  if (Array.isArray(array)) {
+    output = array.filter((item) => item.location === city);
+  }
+  return output;
 }

e435e91af72b5e25a73cfa56d76ee9e21d8fa915
Jonny Wang <qbs1864@gmail.com>
2021-12-20 18:21:06 +0000

    1 linted

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,10 @@
 // Implement a function getListStudentIds that returns an array of ids from a list of object
 export default function getListStudentIds(array) {
-  let output = [];
-  if (Array.isArray(array))
-  {
-    array.forEach(item => {
+  const output = [];
+  if (Array.isArray(array)) {
+    array.forEach((item) => {
       output.push(item.id);
-    })
+    });
   }
   return output;
 }

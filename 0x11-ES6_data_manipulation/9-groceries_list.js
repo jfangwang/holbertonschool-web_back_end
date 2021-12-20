32178b5385183f16b46d4e5d333f2b8f3bd29d9a
Jonny Wang <qbs1864@gmail.com>
2021-12-20 19:44:08 +0000

    9 doen

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+// Implement a function groceriesList that returns a map of
+// groceries with the following items (name, quantity)
+export default function groceriesList() {
+  let map = new Map([
+    ["Apples", 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ])
+  return (map);
+}

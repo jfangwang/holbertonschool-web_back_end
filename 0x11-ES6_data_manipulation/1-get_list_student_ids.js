e1185bc8f13ad22d2675e8e03791621e574b4b11
Jonny Wang <qbs1864@gmail.com>
2021-12-21 05:17:38 +0000

    1 done

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,7 @@
 // Implement a function getListStudentIds that returns an array of ids from a list of object
 export default function getListStudentIds(array) {
-  const output = [];
   if (Array.isArray(array)) {
-    array.forEach((item) => {
-      output.push(item.id);
-    });
+    return array.map((item) => item.id);
   }
-  return output;
+  return ([]);
 }
---
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,8 @@
+// Implement a function queryAPI
+export const weakMap = new WeakMap();
+
+export function queryAPI(endPoint) {
+  if (weakMap.has(endPoint)) {
+    
+  }
+}
